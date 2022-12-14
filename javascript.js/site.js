a97185fb2455525dade598aeb7586ada01f90a9a
Diego Koch <d-koch@hotmail.com>
2022-12-14 11:16:53 -0300

    Borre js del profe

--- javascript.js/site.js
+++ javascript.js/site.js
@@ -1,182 +1,135 @@
 
+//Pre entrega 2 
+
+function inicio() {
+  //Pido el nombre al user
+  let user = prompt("¡Hola! Ingrese su nombre por favor: ");
+  //Estoy diciendo que si no ingresa nada vuelva a pedir el nombre
+  if (user === "") {
+    inicio()
+  };
+  //Ejecuto el saludo y lo dejo en la consola
+  let saludo = console.log(" Bienvenido " + user + " espero que tenga una linda experiencia en Kalu.");
+
+}
+
+inicio();
+
+
+//Creando mis cervezas
+class Cervezas {
+  constructor(nombre, descripcion, precio) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+  }
+}
+
+//Hago las constantes
+
+const cerveza1 = new Cervezas("Dorada Patagonica", "Es una cerveza rubia, Muy suave", 500);
+const cerveza2 = new Cervezas("IPA", "Es una cerveza media anaranjada, Esta en el medio entre suave y fuerte de las mejores a mi criterio ", 600);
+const cerveza3 = new Cervezas("Imperial Stout", "Es una cerveza negra, Es mas fuerte con gusto a cafe", 600);
+const cerveza4 = new Cervezas("Scotish Red", "Es una cerveza roja, Es fuerte pero de las mas pedidas por el publico", 500);
+
+
+//Creo el array + las pusheo
+const arraysCervezas = [];
+
+arraysCervezas.push(cerveza1);
+arraysCervezas.push(cerveza2);
+arraysCervezas.push(cerveza3);
+arraysCervezas.push(cerveza4);
+
+console.log(arraysCervezas);
+
+let opcion = Number("");
+//Hago el menu
+console.log("Bienvenido a la Cerveceria Kalu");
+
+function menu() {
+
+ opcion = parseInt(prompt("Ingrese la opcion: \n 1)Ver menu otra vez. \n 2)Consultar precios.  \n 3)Elegir cerveza.    \n 4)Salir."))
  
- /* console.log
-  para dejar un mensaje en la consola */
 
- /* para dejar comentario en mas 
- de un renglon
- */ 
+//Ejecuto las opciones del menu 
 
- // para dejar un comentario en una sola linea
+//Opcion 1
 
- //typeof para saber que tipo de variable es (en la consola)
-
- //contatenar juntar 2 variables (numericas + frases o v/f)
-
- //declaracion
- //let nombre;
-
- //asignacion
- //let nombre = "Pablo";
-
- //se puede declarar y despues asignar o hacer las dos juntas 
-
-/* let nombre =  prompt('Ingrese su nombre');
-let apellido =  prompt('Ingrese su apellido');;
-let edad =  prompt('Ingrese su edad');;
-
-if(nombre != '' || apellido != '' && edad >= 20) {
-  console.log(`Hola, ${nombre} ${apellido}, tenés ${edad} años`);
-} else {
-  console.log(`Hola ${nombre} ${apellido} tu edad actual es ${edad}`);
-}; */
-
-
-// Cambio de dolar a peso
-
-/*  Ejercicio clase 1
-const valorDolar = 298;
-
-let montoIngresado = prompt ( "Ingrese el monto a comprar dolares ($A " + valorDolar + " c/u)" );
-
-let dolarCompra = Number(montoIngresado) / valorDolar;
-
-alert ( "Ustede a comprado " + parseFloat(dolarCompra).toFixed(2) + " dolares amercianos")
-
-console.log( "Ustede a comprado " +  parseFloat(dolarCompra).toFixed(2) + " dolares amercianos")
-
-*/
-
-
-/* for ( let i=0; i<10 ; i++) {
-console.log ("esta es la  " + i + " vez que pasa por esta linea")
+function verMenu() {
+  i--;
+  menu();
 }
 
-for (let i=)
- */
-/* toUpperCase() mayusculas
-toLowerCase() minusculas
- */
-
-/*
-funcion flecha
-const suma = (a , b)  => { return (a + b)}
-const resta = (a , b)  => { return (a - b)}
-*/
-
-// Presentacion; Entrega 1 Inicio
-
-
-let nombreIngresado = prompt ("Ingrese su nombre por favor.");
-let apellidoIngresado = prompt ("Ingrese su apellido por favor.");
-
-
- function saludarConNombreYApellido (nombre, apellido) {
-  console.log("Hola " + nombre + " " + apellido);
-}
-
-saludarConNombreYApellido(nombreIngresado, apellidoIngresado);
-
-console.log ("Vamos a hacer una calculadora")
-
-
-operacion = prompt("Elija la operación matemática que desea aprender: suma, resta, multiplicacion, division");
-numero1 = parseFloat(prompt("Ingrese el primer numero"))
-numero2 = parseFloat(prompt("Ingrese el segundo numero"))
-
-function calculadora(numero1, operacion, numero2){
-  switch (operacion){
-    case "suma":
-      console.log("Usted eligio hacer una suma")
-      return numero1 + numero2;
-      break;
-      
-      case "resta-":
-        console.log("Usted eligio hacer una resta")
-        return numero1 - numero2;
-        break
-        
-        case "division":
-          console.log("Usted eligio hacer una division")
-          return numero1 / numero2;
-          break
-          
-          case "multiplicacion":
-          console.log("Usted eligio hacer una multiplicacion")
-          return numero1 * numero2;
-          break
-
-          default:
-            return 0;
-            break;
-
+for (i = 0; i <= 1; i++){
+  if (opcion == 1) {
+     verMenu();
   }
 }
 
-let resultado = calculadora(numero1, operacion , numero2)
+//Opcion 2
 
-console.log("Tu resultado es = " + resultado); 
-
-
-
-
- // Listado de presentes
-for ( let i=0; i<10; i++ ) {
-  // En cada repetición solicitamos un nombre.
-  let ingresarNombre = prompt("Ingresar nombre");
-  // Informamos el turno asignado usando el número de repetición (i).
-  console.log(" Turno N° " + (1 + i) + " Nombre: " + ingresarNombre);
-} 
-
-
-prompt("¡Vamos a preguntar por el clima!")
-
- function consultaClima(){
-  let dia = toLowerCase(prompt("¿De que dia queres saber el clima?"));
-
-  switch (dia) {
-
-    case "lunes":
-      prompt("Van a haber 15 º")
-      break;
-
-    case "martes":
-      prompt("Van a haber 21 º")
-      break;
-
-    case "miercoles":
-      prompt("Van a haber -2 º")
-      break;
-
-    case "jueves":
-      prompt("Van a haber 12 º")
-      break;
-
-    case "viernes":
-      prompt("Van a haber 40 º")
-      break;
-
-    case "sabado":
-      prompt("Van a haber 9 º")
-      break;
-
-    case "domingo":
-      prompt("Van a haber 5 º")
-      break;
-
-      default:
-      prompt("Dia incorrecto")
-  }
+function verSoloPrecio(){
+  console.log("El precio de la Dorada Patagonica es " + cerveza1.precio);
+  console.log("El precio de la IPA es " + cerveza2.precio);
+  console.log("El precio de la Imperial Stout es " + cerveza3.precio);
+  console.log("El precio de la Scotish Red es " + cerveza4.precio);
+  do {
+    if (opcion == 2){
+    verMenu();
+    } else {
+      opcion =!
+      console.log("Opcion invalida")
+    }
+       } while (opcion == 2);
 }
-console.log (consultaClima()) 
 
 
-/* EJEMPLO WHILE
-let entrada1 = prompt("Ingresar un dato"); //Repetimos con While hasta que el usuario ingresa "ESC"
-while(entrada1 != "ESC" && entrada1 != "kbz") {
-    // alert("El usuario ingresó " + entrada);
-    //Volvemos a solicitar un dato. En la próxima iteración se evalúa si no es ESC.
-    entrada1 = prompt("Ingresar otro dato");
-} */
+if (opcion == 2) {
+    verSoloPrecio();
+}
+
+
+//Opcion 3
+function elegirCerveza() {
+  eligiendoCerveza = prompt("¿Que cerveza vas a elegir? \n 1)Dorada Patagonica \n 2)IPA \n 3) Imperial Stour \n 4)Scotish Red");
+
+  let pregunta = prompt("¿Desea elegir otra cerveza mas? \n Responda con Si/No")
+    do {
+    if (pregunta == "No") {
+      menu();
+    } else  {
+      (pregunta == "Si");
+      return elegirCerveza();
+    }  
+    } while (eligiendoCerveza == 0 || eligiendoCerveza == "No");
+      return verMenu();
+}
+
+if (opcion == 3) {
+    elegirCerveza();
+}
+
+
+//Opcion 4
+if (opcion == 4 ){
+do {
+  console.log ("Gracias por visitar nuestra pagina, esperamos que le haya gustado")
+} while (opcion =! 4);} 
+}
+
+
+
+
+menu();
+
+
+
+
+
+
+
+
+
 
 
